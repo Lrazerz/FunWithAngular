df6d8d84852712b78058f93242fb9ce918cc38b2
Bogdan Zhuk <35612327+Zhukbo@users.noreply.github.com>
2020-10-06 21:46:20 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductsService } from './products.service'
-
+//Petro Kryvorotko
 @NgModule({
   imports:      [ BrowserModule, HttpClientModule, FormsModule ],
   providers: [ ProductsService ],
